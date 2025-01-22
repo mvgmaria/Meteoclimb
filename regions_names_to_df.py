3eabdae0debb80fcd0a1e7ae8a9742253c7fe1f1
Maria <mvg989898@gmail.com>
2025-01-22 17:02:09 +0100

    Final changes on the main csv, ready to migrate to MySQL

--- regions_names_to_df.py
+++ regions_names_to_df.py
@@ -9,21 +9,28 @@ main_csv = os.path.join(base_dir, "regions_csv\_crag_coordinates.csv")
 output_file = main_csv.replace("_crag_coordinates.csv", "_crag_coordinates_new.csv")
 regions_csv = os.path.join(base_dir, "spain_regions_urls_names.csv")
 
-df_main = pd.read_csv(main_csv)
+df_main = pd.read_csv(main_csv, sep="\t")
 df_region = pd.read_csv(regions_csv)
 df_main_ = pd.DataFrame(df_main)
 df_region_ = pd.DataFrame(df_region)
-print(df_region_.columns)
 
-region_dict = df_region_.set_index("Initial region Names")["Region URLs"].to_dict()
+
+region_dict = df_region_.set_index("Region URLs")["Initial region Names"].to_dict()
+print(region_dict)
+
+region_name_list = []
 
 for i, row in df_main_.iterrows():
-    url = row[
-        "Initial Region URL"
-    ]  # Asumiendo que esta columna contiene las URLs en df_main
+    # print(f"This is i: {str(i)}")
+    # print(f"This is row: {str(row)}")
+    url = row["Initial Region URL"]
+    # print(url)
     if url in region_dict:
-        df_main_.at[i, "Region Name"] = region_dict[
-            url
-        ]  # Asignar el nombre de la región
+        # print("True")
+        region_name_list.append(region_dict[url])
+
+print(region_name_list)
+
+df_main_.insert(0, "Initial Region Name", region_name_list)
 
 df_main_.to_csv(output_file, index=False)
