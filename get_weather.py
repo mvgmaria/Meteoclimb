c40e39b2b0747d71651e7a79bde904506ab3c4c3
Maria <mvg989898@gmail.com>
2025-02-26 21:14:34 +0100

    Corrected request url for weather data

--- get_weather.py
+++ get_weather.py
@@ -4,7 +4,7 @@ import os
 from datetime import datetime
 from pprint import pprint
 
-load_dotenv()
+load_dotenv(dotenv_path=".climbproject\.env")
 
 
 # enables the env variables so we can retrieve them
@@ -18,20 +18,17 @@ def get_weather(lat, lon):
     )  # i would have to check the format of the datetime function
     print(cnt)
 
-    request_url = f'https://api.openweathermap.org/data/2.5/forecast/daily?lat={lat}&lon={lon}&cnt={cnt}&appid={os.getenv("API_KEY_W")}&units=metric'  # here we have to add the date argument in the url
+    request_url = f'https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={os.getenv("API_KEY_W")}'
 
     weather_data = requests.get(request_url).json()
 
     pprint(weather_data)
-    # pprint as in "pretty print" rearranges the data so it is more readable
 
-    print(f'\nCurrent Weather for {weather_data["name"]}.')
-    print(f'\nThe temp is {weather_data["main"]["temp"]}.')
-    print(
-        f'\nFeels like {weather_data["main"]["feels_like"]} and {weather_data["weather"][0]["description"]}.'
-    )
-    # when we state {weather_data["name"]} we are searching in the json file and taking that parameter, but in this way it has better readibilty
-    # {weather_data["main"]["temp"]} is referring to main group and inside the temp (we can see in the json data)
+    # print(f'\nCurrent Weather for {weather_data["name"]}.')
+    # print(f'\nThe temp is {weather_data["main"]["temp"]}.')
+    # print(
+    #     f'\nFeels like {weather_data["main"]["feels_like"]} and {weather_data["weather"][0]["description"]}.'
+    # )
 
 
 if __name__ == "__main__":
